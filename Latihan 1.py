79d5c455137255593173be2897a404778a170fd6
ANIMATION-14 <animation-14@ANIMATION-14s-iMac.local>
2026-01-07 11:25:38 +0700

    Update Latihan 1

---
+++ Latihan 1.py
@@ -0,0 +1,19 @@
+a = "Ayam_Bakar"
+b = "Ayam_Goreng"
+#swap values
+
+a, b = b, a 
+print(a)
+print(b)
+
+
+tebak_aku = ['a', 'b', 'c','d']
+user_input = input ("Masukan aku: ")
+
+if user_input in tebak_aku:
+    print("input ada di dalam list tebak_aku")
+else:
+    print("input tidak ada di dalam list tebak_aku")
+
+
+
