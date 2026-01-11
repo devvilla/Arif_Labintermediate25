c64782e191465be176195203e0e05c28e9b89b9b
Muhammad Arif Rahman <arip@MacBook-Air-Muhammad.local>
2026-01-11 14:51:44 +0700

    ArifLabPertama_WithKakaNoelGanteng.py

--- test.py
+++
@@ -1,9 +0,0 @@
-# Kalkulator sederhana
-angka1 = float(input("Masukkan angka pertama: "))
-angka2 = float(input("Masukkan angka kedua: "))
-
-print(f"\nHasil:")
-print(f"Penjumlahan: {angka1 + angka2}")
-print(f"Pengurangan: {angka1 - angka2}")
-print(f"Perkalian: {angka1 * angka2}")
-print(f"Pembagian: {angka1 / angka2}")
